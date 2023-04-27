cdc622666e3c2e82ea7feffb37806af484890b08
VictorGutierrez <vagutierrezv@uqvirtual.edu.co>
2023-04-27 16:42:21 -0500

    Funcion flecha explicacion

---
+++ Node/02-fundamentosJs/flecha.js
@@ -0,0 +1,19 @@
+// function sumar(a ,b) {
+//     return a + b;
+// }
+
+
+const sumar = (a,b ) =>  a + b;
+
+
+// const sumar = (a,b ) => {
+//     return a + b;
+// }
+
+// const sumar = (a,b = 10) => {
+//     return a + b;
+// }
+
+console.log(sumar(5,5));
+
+
