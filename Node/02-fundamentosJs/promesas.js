1754c74a10acaf0bb7b3674f39d1d2643e76e7e0
VictorGutierrez <vagutierrezv@uqvirtual.edu.co>
2023-04-28 10:02:24 -0500

    Promesas reduccion codigo

--- Node/02-fundamentosJs/promesas.js
+++ Node/02-fundamentosJs/promesas.js
@@ -1,61 +1,46 @@
 const empleados = [
-    {
-        id: 1,
-        nombre: 'Fernando'
-    },
-    {
-        id: 2,
-        nombre: 'Linda'
-    },
-    {
-        id: 3,
-        nombre: 'Karen'
-    }
+  {
+    id: 1,
+    nombre: "Fernando",
+  },
+  {
+    id: 2,
+    nombre: "Linda",
+  },
+  {
+    id: 3,
+    nombre: "Karen",
+  },
 ];
 
 const salarios = [
-    {
-        id: 1,
-        salario: 1000
-    },
-    {
-        id: 2,
-        salario: 1500
-    }
+  {
+    id: 1,
+    salario: 1000,
+  },
+  {
+    id: 2,
+    salario: 1500,
+  },
 ];
 
+const getEmpleado = (id) => {
+  return new Promise((resolve, reject) => {
+    const empleado = empleados.find((e) => e.id === id)?.nombre;
 
-
-const getEmpleado = ( id ) => {
-
-    
-    return new Promise((resolve, reject) => {
-        
-        const empleado = empleados.find( e => e.id === id )?.nombre;
-
-         (empleado) 
-          ? resolve (empleado)
-          : reject(`No existe empleado con id ${id}`);
-
-    });
-}
+    empleado ? resolve(empleado) : reject(`No existe empleado con id ${id}`);
+  });
+};
 
 const getSalario = () => {
+  return new Promise((resolve, reject) => {
+    const salario = salarios.find((s) => s.id === id)?.salario;
 
-    return new Promise((resolve, reject) => {
+    salario ? resolve(salario) : reject(`No existe el salario con id ${id}`);
+  });
+};
 
-        const salario = salarios.find( s =>  s.id === id)?.salario;
-
-        (salario)
-         ? resolve(salario)
-         : reject(`No existe el salario con id ${id}`);
-        
-    });
-
-} 
-
-
-const id = 1;
+const id = 10;
 
 // getEmpleado(id)
 //     .then (empleado  => console.log( empleado))
@@ -63,16 +48,14 @@ const id = 1;
 
 // getSalario(id)
 //     .then (salario  => console.log( salario))
-//     .catch (err => console.log(err));    
+//     .catch (err => console.log(err));
+
+let nombre;
 
 getEmpleado(id)
-    .then( empleado =>{
-
-        getSalario(id)
-          .then (salario => {
-             console.log('El empleado:', empleado, 'tiene un salario de:',salario);
-          })
-          .catch(err => console.log(err))
-
-    })
-    .catch(err => console.log(err))
+  .then((empleado) => {
+    nombre = empleado;
+    return getSalario(id);
+  })
+  .then((salario) => console.log("El empleado:", nombre, "tiene un salario", salario))
+  .catch(err => console.log(err));
