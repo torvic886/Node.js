54531e2072e8d69c6a9e27867d4ade3d13932ac2
VictorGutierrez <vagutierrezv@uqvirtual.edu.co>
2023-04-28 10:48:01 -0500

    Implementacion For

---
+++ Node/02-fundamentosJs/app.js
@@ -0,0 +1,14 @@
+
+// for (let i = 1; i <= 10; i++) 
+// {
+//     console.log(`5 X ${i} = ${5 * i}`);
+    
+// }
+
+const base = 5;
+
+for (let i = 1; i <= 10; i++) 
+{
+    console.log(`${base} X ${i} = ${base*i}`);
+    
+}
