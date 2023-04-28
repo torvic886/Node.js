43aa1c7c86e9c224bb4a8bd8ef5db6819e36e60f
VictorGutierrez <vagutierrezv@uqvirtual.edu.co>
2023-04-28 11:20:44 -0500

    Permiso de escritura en Node, se realiza implementacion con las tablas de multiplicar

--- Node/03-bases-node/app.js
+++ Node/03-bases-node/app.js
@@ -1,7 +1,25 @@
-const base = 5;
+const fs = require('fs');
+
+console.clear();
+const base = 7;
+
+console.log('==============');
+console.log(`tabla-${ base }.txt`);
+console.log('==============');
+
+
+let salida='';
 
 for (let i = 1; i <= 10; i++) 
 {
-    console.log(`${base} X ${i} = ${base*i}`);
+    salida+=(`  ${base} X ${i} = ${base*i}\n`);
     
-}
+} 
+
+console.log(salida);
+
+fs.writeFile( `tabla-${ base }.txt`,salida, (err) => {
+    if (err) throw err;
+
+    console.log(`tabla-${ base }.txt creada`);
+});
