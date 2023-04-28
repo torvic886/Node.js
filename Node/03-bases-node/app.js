397731290cb7c3aac45e431f9a31786b0ef14fd0
VictorGutierrez <vagutierrezv@uqvirtual.edu.co>
2023-04-28 12:25:14 -0500

    Realizar Operaciones desde CMD

--- Node/03-bases-node/app.js
+++ Node/03-bases-node/app.js
@@ -3,7 +3,14 @@ const { crearArchivo } = require('./helpers/multiplicar')
 
 console.clear();
 
-const base = 5;
+console.log(process.argv);
+
+const[,,arg3 = 'base=5'] = process.argv;
+const [,base] = arg3.split('=')
+
+console.log(base);
+
+//const base = 5;
 
 crearArchivo( base )
     .then(nombreArchivo => console.log(nombreArchivo))
